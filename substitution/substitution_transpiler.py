3012a7df38305b9a940f937d58bc6476cb0d8c82
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2020-04-25 11:57:02 -0400

    Closes #12, add method for shellcode to asm

---
+++ substitution/substitution_transpiler.py
@@ -0,0 +1,26 @@
+"""
+The transpiler module is responsible for converting shellcode into x86 asm and
+vice versa.
+"""
+
+import os
+import sys
+
+
+MODULE_DIR_NAME = os.path.dirname(os.path.realpath(__file__))
+if MODULE_DIR_NAME not in sys.path:
+    sys.path.insert(0, MODULE_DIR_NAME)
+
+
+from pwn import context, disasm, unhex
+
+
+context.arch = 'i386'
+context.os   = 'os'
+
+
+def shellcode_to_asm(shellcode, raw_hex=True):
+    'Convert shellcode into x86 asm code.  If raw_hex is False, need to unhex it first.'
+    if raw_hex:
+        return disasm(shellcode, byte=False, offset=False).split('\n')
+    return disasm(unhex(shellcode), byte=False, offset=False).split('\n')
