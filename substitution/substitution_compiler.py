c5f9305cd794ffa8f90cb4ec5e14cfcbe52986b2
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2020-04-26 17:46:56 -0400

    Fixes #9, implement check_coverage for compiler

--- substitution/substitution_compiler.py
+++ substitution/substitution_compiler.py
@@ -28,9 +28,27 @@ class Compiler():
         Associate a filename with the given operator and operands from which
         valid substitutions would be parsed.
         """
-        operands_key = ','.join([operand.name for operand in operands])
+        operands_key = ', '.join([operand.name for operand in operands])
 
         if operator.name in self.signatures:
             self.signatures[operator.name][operands_key] = filename
         else:
             self.signatures[operator.name] = {operands_key: filename}
+
+
+    def check_coverage(self, parse_data):
+        """
+        Check to see if there is a signature for every command in the parse data and
+        print any commands that are not covered.
+        """
+        incomplete_coverage = True
+
+        for parse_annotation in parse_data:
+            operator, operands = parse_annotation[0].name, parse_annotation[1:]
+            operands_key = ', '.join([operand.kind.name for operand in operands])
+
+            if operator not in self.signatures or operands_key not in self.signatures[operator]:
+                incomplete_coverage = False
+                print(f'Missing signature for {operator} {operands_key}')
+
+        return incomplete_coverage
