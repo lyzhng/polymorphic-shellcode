762561aad75e5d7ca5a0e90cfff7fdd04f81027d
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2020-04-28 20:40:32 -0400

    Add RegexSwitch for pattern matching against many patterns

---
+++ substitution/substitution_utils.py
@@ -0,0 +1,25 @@
+"""
+The utils module provide some functions for syntactic sugar.
+"""
+
+class RegexSwitch:
+
+    """
+    A class used to mimic the switch statement in other languages.  This one operates
+    on regular expressions.
+    """
+
+    def __init__(self, operand_type):
+        self.operand_type = operand_type
+
+
+    def __enter__(self):
+        return self
+
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        return False
+
+
+    def __call__(self, pattern):
+        return pattern.match(self.operand_type)
