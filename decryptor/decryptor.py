8b25a46d7a006a64a3fe0402cda8d00bf4f50481
Lily Zhong <lily.zhong@stonybrook.edu>
2020-04-28 20:36:34 -0400

    Add functionality to run shellcode

--- decryptor/decryptor.py
+++ decryptor/decryptor.py
@@ -1,5 +1,5 @@
 """
-decryptor.py generates a c program by reading in template.c and filling in the shellcode for _TARGET.
+decryptor.py generates a C program by reading in template.c and filling in the shellcode to replace _TARGET.
 After generating, the program gets compiled and is executed.
 """
 
@@ -7,19 +7,24 @@ After generating, the program gets compiled and is executed.
 import sys
 import os
 from typing import List
+import argparse
 
 
-_TARGET: str = 'const unsigned char encrypted_execve_sc[];'  
+_EXECUTOR_TARGET: str = 'const unsigned char code[];'
+_DECRYPTOR_TARGET: str = 'const unsigned char encrypted_execve_sc[];'  
 _NL: str = '\n'  
 
 
 def fill_template(sc: str) -> str:
 	builder: List[str] = []
-	with open('template.c', 'r') as f:
+	file_to_read: str = 'templates/decryptor_template.c' if args.decrypt else 'templates/executor_template.c'
+	with open(file_to_read, 'r') as f:
 		for line in f:
 			line = line.lstrip()
-			if line.rstrip() == _TARGET:
+			if args.decrypt and line.rstrip() == _DECRYPTOR_TARGET:
 				builder.append(f'const unsigned char encrypted_execve_sc[] = "{sc}";{_NL}')
+			elif args.execute and line.rstrip() == _EXECUTOR_TARGET:
+				builder.append(f'const unsigned char code[] = "{sc}";{_NL}')
 			else:
 				builder.append(line)
 	return ''.join(builder)
@@ -35,7 +40,21 @@ def execute_program() -> None:
     os.system('./output')
 
 
+def args_exist() -> bool:
+	return any(v for _, v in vars(args).items())
+
+
 if __name__ == '__main__':
-	builder = fill_template(sys.argv[1])
-	write_file(builder)
-	execute_program()
+	parser = argparse.ArgumentParser(description='Generate a C program by reading in a template and filling in the shellcode. After generating, the program gets compiled and is executed.')
+	parser.add_argument('-d', '--decrypt', help='Generate a C program that decrypts the shellcode and then executes it')
+	parser.add_argument('-x', '--execute', help='Generate a C program that executes shellcode')
+	args = parser.parse_args()
+	if args_exist():
+		if args.decrypt:
+			builder = fill_template(args.decrypt)
+		if args.execute:
+			builder = fill_template(args.execute)
+		write_file(builder)
+		execute_program()
+	else:
+		parser.print_help()
