7b284bcb4712292ff425dbe9919934280f21ce62
Patrick Chan <16124030+pchan37@users.noreply.github.com>
2020-05-09 22:34:37 -0400

    Modify driver to insert encrypted_sc, iv, key

--- final/main.py
+++ final/main.py
@@ -3,11 +3,15 @@ import re
 
 import parser
 from compiler import Compiler
+from utils import RegexSwitch
 
 _ENCRYPTED_SC = b''
 _IV = b''
 _KEY = b''
 
+_ENCRYPTED_SC_TARGET_REGEX = re.compile(r'{{ ENCRYPTED_SC }}')
+_IV_TARGET_REGEX = re.compile(r'{{ IV }}')
+_KEY_TARGET_REGEX = re.compile(r'{{ KEY }}')
 _TEMPLATE_TARGET_REGEX = re.compile(r'[\t ]*{{ ([\w\d_]+) }}[\t ]*')
 
 
@@ -17,17 +21,29 @@ def main():
     
     with open('template.s') as template:
         for line in template:
-            match = _TEMPLATE_TARGET_REGEX.fullmatch(line.strip())
-            if match is not None:
-                template_filename: str = f'{match.group(1)}.template'
-                operand_to_type_mapping, code = parser.preprocess(template_filename)
-                annotations: List[Annotation] = parser.parse(operand_to_type_mapping, code)
-                rewritten_program: str = compiler.apply_substitution(annotations)
-                rewritten_program = '\n'.join(map(lambda x: f'        {x}', rewritten_program.split('\n')))
-                final_program += rewritten_program
-                final_program += '\n'
-            else:
-                final_program += line
+            with RegexSwitch(line.strip()) as case:
+                if case(_ENCRYPTED_SC_TARGET_REGEX):
+                    final_program += f'        .string "{_ENCRYPTED_SC}"\n'
+                elif case(_IV_TARGET_REGEX):
+                    final_program += f'        .ascii "{_IV}"\n'
+                elif case(_KEY_TARGET_REGEX):
+                    key_part_one: str = _KEY[:21]
+                    key_part_two: str = _KEY[21:]
+                    final_program += f'        .ascii "{key_part_one}"\n'
+                    final_program += f'        .ascii "{key_part_two}"\n'
+                elif case(_TEMPLATE_TARGET_REGEX):
+                    match = _TEMPLATE_TARGET_REGEX.fullmatch(line.strip())
+                    if match is not None:
+                        template_filename: str = f'{match.group(1)}.template'
+                        operand_to_type_mapping, code = parser.preprocess(template_filename)
+                        annotations: List[Annotation] = parser.parse(operand_to_type_mapping, code)
+                        rewritten_program: str = compiler.apply_substitution(annotations)
+                        rewritten_program = '\n'.join(map(lambda x: f'        {x}', rewritten_program.split('\n')))
+                        final_program += rewritten_program
+                        final_program += '\n'
+                else:
+                    final_program += line
+            
     print(final_program)
 
 
