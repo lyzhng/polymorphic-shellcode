2179df7867530d1b570d8337af01bf5721050d3b
Lily Zhong <lily.zhong@stonybrook.edu>
2020-05-13 21:30:29 -0400

    Fill in run_process

--- entropy_analysis/byte_freq.py
+++ entropy_analysis/byte_freq.py
@@ -1,42 +1,48 @@
 import os
+import sys
 import matplotlib.pyplot as plot 
 
 
 freq = {}
 
 def run_process():
-    # need to apply substitution to the program here
-    os.system(r'for i in `objdump -D -EL output | tr "\t" " " | tr " " "\n" | egrep "^[0-9a-f]{2}$"`; do echo -n "$i"; done > hex.txt')
+    os.system(r'python3 main.py -f example.sc > .temp && tail -n +2 .temp > output.sc && rm .temp')
 
 
 def read_file(filename):
-	builder = []
-	with open(filename, 'r') as f:
-		for line in f.readlines():
-			line = line.strip()
-			builder.append(line.strip())
-	return ''.join(builder)
+    builder = []
+    with open(filename, 'r') as f:
+        for line in f.readlines():
+            line = line.strip()
+            builder.append(line.strip())
+    return ''.join(builder)
 
 
 def plot_graph():
-	plot.bar(list(freq.keys()), freq.values(), color='b')
-	plot.show()
+    plot.bar(list(freq.keys()), freq.values(), color='b')
+    plot.suptitle('Hex (in decimal) vs. Frequency', fontsize=16)
+    plot.xlabel('Hex (in decimal)', fontsize=12)
+    plot.ylabel('Frequency', fontsize=12)
+    plot.show()
 
 
 def main():
-	for i in range(1):
-		run_process()
-		builder = read_file('hex.txt');
-		hexes = [builder[j:j+2] for j in range(0, len(builder), 2)]
-		# print(hexes)
-		for _hex in hexes:
-			dec = int(_hex, 16)
-			if dec > 255 or dec < 0:
-				print('Decimal is not between 0 and 255.')
-				return
-			freq.setdefault(dec, 0);
-			freq[dec] += 1
-	plot_graph()
+    owd = os.getcwd()
+    os.chdir('..')
+    for i in range(1024):
+        print('Iteration', i)
+        run_process()
+        builder = read_file('output.sc')
+        hexes = [builder[j:j+2] for j in range(0, len(builder), 2)]
+        for _hex in hexes:
+            dec = int(_hex, 16)
+            if dec > 255 or dec < 0:
+                print('Decimal is not between 0 and 255.')
+                sys.exit(1)
+            freq.setdefault(dec, 0);
+            freq[dec] += 1
+    os.chdir(owd)
+    plot_graph()
 
 
 main()
--- entropy_analysis/to_bmp.py
+++ entropy_analysis/to_bmp.py
@@ -8,7 +8,7 @@ matrix = []
 
 
 def run_process():
-    os.system('make clean && make')
+    os.system(r'python3 main.py -f example.sc > .temp && tail -n +2 .temp > output.sc && rm .temp')
 
 
 def read_file(filename):
@@ -28,24 +28,25 @@ def display_image():
     im = np.array(matrix)
     # im = np.matrix(matrix)
     plt.imshow(im, cmap='gray')
-    plt.axis('off')
+    # plt.axis('off')
     plt.savefig('pict.png', bbox_inches='tight', pad_inches = 0)
     # plt.gca().set_axis_off()
-    plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
-    plt.margins(0,0)
+    # plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
+    # plt.margins(0,0)
     plt.show()
 
 
 def main():
-    for i in range(512):
-        # run_process()
-        # hexes = read_file('hex_copy.txt')
-        hexes = []
-        hexes = [randrange(0, 255) for j in range(1024)]
-        # dec = [int(_hex, 16) for _hex in hexes]
-        matrix.append(hexes)
-        print(f'The length of each row is {len(hexes)}')
+    owd = os.getcwd()
+    os.chdir('..')
+    for i in range(50):
+        print('Iteration', i)
+        run_process()
+        hexes = read_file('output.sc')
+        dec = [int(h, 16) for h in hexes]
+        matrix.append(dec)
     print(f'The length of the matrix is {len(matrix)}')
+    os.chdir(owd)
     display_image()
 
 main()
